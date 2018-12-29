39d2aabbef83dcd1fca2eb4232da8edbb85af6ad
dletk <duc.mtle@gmail.com>
2018-12-29 18:38:15 +0700

    Working version

--- NBKOlympia/NBKOlympia/settings.py
+++ NBKOlympia/NBKOlympia/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 
 # Static files (CSS, JavaScript, Images)
--- NBKOlympia/NBKOlympia/urls.py
+++ NBKOlympia/NBKOlympia/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("userprofile.urls")),
     path('', include("tangtoc.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
